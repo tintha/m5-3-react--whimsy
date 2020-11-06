74490e0063c65d0a59e381e65ea05d93665b4ff8
tintha <tintha@gmail.com>
2020-11-05 23:33:24 -0500

    Just fixed some indentations last exercise

--- src/components/Tweet/Rotate.js
+++ src/components/Tweet/Rotate.js
@@ -5,9 +5,9 @@ const Rotate = ({children}) => {
   const props = useSpring({
     zIndex: 2,
     display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
     transform: 'rotate(360deg)',
     from: { transform: 'rotate(0deg)'},
   })
