d05a2dff2a201521c883097f5c580b474a547563
tintha <tintha@gmail.com>
2020-11-05 21:39:56 -0500

    Ex 6

---
+++ src/components/LikeButton/ScaleIn.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSpring, animated } from "react-spring";
+
+const ScaleIn = ({children}) => {
+  const props = useSpring({
+    zIndex: 2,
+    transform: 'scale(1)',
+    from: { transform: 'scale(0)'},
+    config: {
+      tension: 200,
+      friction: 10,
+      mass: 2,
+      velocity: 5
+    },
+  })
+  return <animated.div style={props}>{children}</animated.div>
+} 
+
+export default ScaleIn;
